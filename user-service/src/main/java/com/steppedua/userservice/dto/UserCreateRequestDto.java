4abe8f5ee29d97a812a9f51338160ec2e90eea13
Eduard Stepanyan <stepanyan.edua@gmail.com>
2023-07-11 16:22:19 +0300

    Ref/005 Add TestContainers

--- user-service/src/main/java/com/steppedua/userservice/dto/UserCreateRequestDto.java
+++ user-service/src/main/java/com/steppedua/userservice/dto/UserCreateRequestDto.java
@@ -1,8 +1,10 @@
 package com.steppedua.userservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class UserCreateRequestDto {
     private String firstName;
     private String lastName;
---
+++ user-service/src/test/java/com/steppedua/userservice/service/UserServiceTest.java
@@ -0,0 +1,67 @@
+package com.steppedua.userservice.service;
+
+import com.steppedua.userservice.UserRepository;
+import com.steppedua.userservice.dto.UserCreateRequestDto;
+import com.steppedua.userservice.tables.pojos.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.OffsetDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.mockito.BDDMockito.given;
+
+@SpringBootTest
+@Testcontainers
+class UserServiceTest {
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15.3-alpine")
+            .withDatabaseName("test")
+            .withUsername("postgres")
+            .withPassword("postgres")
+            .withExposedPorts(5432);
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+    private String uuid;
+    private User user;
+
+    @BeforeEach
+    public void init() {
+        uuid = "66e644eb-e4c3-4c2d-9d0d-ca0a19437fd4";
+        user = new User(UUID.fromString(uuid),
+                "ACTIVE", "Ivan", "Ivanov", OffsetDateTime.now(), null);
+    }
+
+    @Test
+    void shouldCreateUserSuccessfully() {
+        final var userCreateRequestDto = new UserCreateRequestDto("Ivan", "Ivanov");
+
+        given(userRepository.createUser(userCreateRequestDto)).willReturn(UUID.fromString(uuid));
+
+        final var userUUID = userService.createUser(userCreateRequestDto);
+        Assertions.assertThat(userUUID).isEqualTo(UUID.fromString(uuid));
+    }
+
+    @Test
+    void shouldGetUserByIdTest() {
+        given(userRepository.getUserById(UUID.fromString(uuid))).willReturn(Optional.of(user));
+
+        final var userById = userService.getUserById(UUID.fromString(uuid));
+
+        Assertions.assertThat(userById).isNotNull();
+    }
+}
